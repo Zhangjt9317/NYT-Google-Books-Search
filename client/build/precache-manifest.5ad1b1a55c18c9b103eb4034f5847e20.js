162c6499815c23554e02e6417f81bf86a4e14a60
jingtian zhang <jtz9317@gmail.com>
2019-04-12 15:07:03 -0700

    build files

---
+++ client/build/precache-manifest.5ad1b1a55c18c9b103eb4034f5847e20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ced77b211babcd287dea52de2730701",
+    "url": "/static/media/books-2.9ced77b2.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d20685125b33d3c496a",
+    "url": "/static/js/main.39acdcf8.chunk.js"
+  },
+  {
+    "revision": "e52a62c5354288c96da4",
+    "url": "/static/js/2.684f6255.chunk.js"
+  },
+  {
+    "revision": "0d20685125b33d3c496a",
+    "url": "/static/css/main.c459cbaa.chunk.css"
+  },
+  {
+    "revision": "bf515e93722b703b5585d371ac79e585",
+    "url": "/index.html"
+  }
+];
